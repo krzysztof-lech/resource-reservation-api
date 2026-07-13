a1138b540e0517513c41076b60d00492e1afa692
Krzysztof Lech <krzysztof.lech02@gmail.com>
2026-07-13 19:19:48 +0200

    feat: add FluentValidation for updating resources

---
+++ ResourceReservation.Api/Validators/UpdateResourceDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using ResourceReservation.Api.Dtos;
+
+namespace ResourceReservation.Api.Validators;
+
+public class UpdateResourceDtoValidator : AbstractValidator<ResourceUpdateDto>
+{
+    public UpdateResourceDtoValidator()
+    {
+        RuleFor(r => r.Name)
+            .MinimumLength(2).WithMessage("Resource name must be at least 2 characters long.")
+            .MaximumLength(100).WithMessage("Resource name cannot exceed 100 characters.")
+            .When(r => r.Name is not null);
+
+        RuleFor(r => r.SlotDurationMinutes)
+            .GreaterThan(0).WithMessage("Slot duration must be greater than 0 minutes.")
+            .When(r => r.SlotDurationMinutes.HasValue);
+
+        RuleFor(r => r.AvailableTo)
+            .GreaterThan(r => r.AvailableFrom)
+            .WithMessage("Available To time must be after Available From time.")
+            .When(r => r.AvailableTo.HasValue && r.AvailableFrom.HasValue);
+    }
+}
