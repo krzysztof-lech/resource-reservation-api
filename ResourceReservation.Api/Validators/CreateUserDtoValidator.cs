f86fc6aa2e0b5e65883e649aee64fbc5e80c48ab
Krzysztof Lech <krzysztof.lech02@gmail.com>
2026-07-13 12:45:59 +0200

    feat: add FluentValidation for creating users

---
+++ ResourceReservation.Api/Validators/CreateUserDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using ResourceReservation.Api.Dtos;
+
+namespace ResourceReservation.Api.Validators;
+
+public class CreateUserDtoValidator : AbstractValidator<UserCreateDto>
+{
+    public CreateUserDtoValidator()
+    {
+        RuleFor(u => u.FirstName)
+            .NotEmpty().WithMessage("First name is required.")
+            .MinimumLength(2).WithMessage("First name must be at least 2 characters long.");
+
+        RuleFor(u => u.LastName)
+            .NotEmpty().WithMessage("Last name is required.")
+            .MinimumLength(2).WithMessage("Last name must be at least 2 characters long.");
+
+        RuleFor(u => u.Email)
+            .NotEmpty().WithMessage("Email address is required.")
+            .EmailAddress().WithMessage("A valid email address is required.");
+
+        RuleFor(u => u.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long.");
+    }
+}
