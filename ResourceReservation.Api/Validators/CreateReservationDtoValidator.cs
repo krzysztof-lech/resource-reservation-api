27cd35c7b31758199b44c737a1b3f7984d94c309
Krzysztof Lech <krzysztof.lech02@gmail.com>
2026-07-13 12:27:54 +0200

    feat: add FluentValidation for creating resources

--- ResourceReservation.Api/Validators/CreateReservationDtoValidator.cs
+++ ResourceReservation.Api/Validators/CreateReservationDtoValidator.cs
@@ -1,23 +1,22 @@
 ﻿using FluentValidation;
 using ResourceReservation.Api.Dtos;
 
-namespace ResourceReservation.Api.Validators
+namespace ResourceReservation.Api.Validators;
+
+public class CreateReservationDtoValidator : AbstractValidator<CreateReservationDto>
 {
-    public class CreateReservationDtoValidator : AbstractValidator<CreateReservationDto>
+    public CreateReservationDtoValidator()
     {
-        public CreateReservationDtoValidator()
-        {
-            RuleFor(x => x.ResourceId)
-                .NotEmpty()
-                .WithMessage("Resource ID is required.");
+        RuleFor(x => x.ResourceId)
+            .NotEmpty()
+            .WithMessage("Resource ID is required.");
 
-            RuleFor(x => x.StartTime)
-                .GreaterThan(DateTime.UtcNow)
-                .WithMessage("Reservation start time must be in the future.");
+        RuleFor(x => x.StartTime)
+            .GreaterThan(DateTime.UtcNow)
+            .WithMessage("Reservation start time must be in the future.");
 
-            RuleFor(x => x.EndTime)
-                .GreaterThan(x => x.StartTime)
-                .WithMessage("Reservation end time must be after the start time.");
-        }
+        RuleFor(x => x.EndTime)
+            .GreaterThan(x => x.StartTime)
+            .WithMessage("Reservation end time must be after the start time.");
     }
 }
---
+++ ResourceReservation.Api/Validators/CreateResourceDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using ResourceReservation.Api.Dtos;
+
+namespace ResourceReservation.Api.Validators;
+
+public class CreateResourceDtoValidator : AbstractValidator<ResourceCreateDto>
+{
+    public CreateResourceDtoValidator()
+    {
+        RuleFor(r => r.Name)
+            .NotEmpty().WithMessage("Resource name is required.")
+            .MinimumLength(2).WithMessage("Resource name must be at least 2 characters long.")
+            .MaximumLength(100).WithMessage("Resource name cannot exceed 100 characters.");
+
+        RuleFor(r => r.SlotDurationMinutes)
+            .GreaterThan(0).WithMessage("Slot duration must be greater than 0 minutes.");
+
+        RuleFor(r => r.AvailableTo)
+            .GreaterThan(r => r.AvailableFrom)
+            .WithMessage("Available To time must be after Available From time.");
+    }
+}
