8943e737f1f04ccf98b8d5f00908df1792b94ad8
Krzysztof Lech <krzysztof.lech02@gmail.com>
2026-07-22 12:50:04 +0200

    test: add TestAuthHandler for integration tests authorization

---
+++ ResourceReservation.Tests/Integration/TestAuthHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace ResourceReservation.Tests.Integration;
+
+public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    public const string Scheme = "Test";
+
+    public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+        : base(options, logger, encoder, clock) { }
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Name, "integration_user")
+        };
+        var identity = new ClaimsIdentity(claims, Scheme);
+        var ticket = new AuthenticationTicket(new ClaimsPrincipal(identity), Scheme);
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
