8b9ca25134e7280b183463f32a8e624c16125d26
Krzysztof Lech <krzysztof.lech02@gmail.com>
2026-07-22 12:30:26 +0200

    test: add CustomWebApplicationFactory with SQLite in-memory

---
+++ ResourceReservation.Tests/Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using ResourceReservation.Api.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResourceReservation.Tests.Integration;
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IDisposable
+{
+    private readonly SqliteConnection _connection;
+
+    public CustomWebApplicationFactory()
+    {
+        _connection = new SqliteConnection("DataSource=:memory:");
+        _connection.Open();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<AppDbContext>));
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlite(_connection);
+            });
+
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            db.Database.EnsureCreated();
+        });
+    }
+
+    public new void Dispose()
+    {
+        base.Dispose();
+        _connection?.Dispose();
+    }
+}
+
